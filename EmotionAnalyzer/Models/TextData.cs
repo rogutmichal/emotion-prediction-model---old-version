afc61332d218aba3e82ef97e571a1fbd7b665be5
rogutmichal <80913819+rogutmichal@users.noreply.github.com>
2026-07-14 20:26:50 +0200

    Add files via upload

---
+++ EmotionAnalyzer/Models/TextData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmotionAnalyzer.Models
+{
+    public class TextData
+    {
+        public string Text { get; set; }
+        public string Emotion { get; set; }
+
+
+    }
+}
