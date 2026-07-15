ddaa851466818158e214fb5aa5e60037b3436a87
rogutmichal <rogutmichal00@gmail.com>
2026-07-15 21:19:10 +0200

    first tests adding

---
+++ EmotionAlazyzerTests/DataLoaderTests.cs
@@ -0,0 +1,48 @@
+﻿using EmotionAnalyzerML;
+using EmotionAnalyzerML.Data;
+using Xunit;
+
+namespace EmotionAnalyzer.Tests;
+
+public class DataLoaderTests
+{
+    [Fact]
+    public void LoadData_ShouldReturnItems()
+    {
+
+        var path = "TestData/test.txt";
+
+
+        var result = DataLoader.LoadDataFromFile(path);
+
+
+        Assert.NotNull(result);
+
+        Assert.NotEmpty(result);
+    }
+
+    [Fact]
+    public void LoadData_ShouldReadCorrectText()
+    {
+        var result =
+            DataLoader.LoadDataFromFile(
+                "TestData/test.txt");
+
+        Assert.Equal(
+            "I love pizza",
+            result.First().Text);
+    }
+
+
+    [Fact]
+    public void LoadData_ShouldReadCorrectEmotion()
+    {
+        var result =
+            DataLoader.LoadDataFromFile(
+                "TestData/test.txt");
+
+        Assert.Equal(
+            "joy",
+            result.First().Emotion);
+    }
+}
